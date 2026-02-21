d40b7c814232c48025090f907741814161894fa9
Dietmar, DL2SBA <pachube@web.de>
2026-02-21 09:46:37 +0100

    Add files via upload

--- Reamp2CSV.py
+++ Reamp2CSV.py
@@ -1,128 +1,242 @@
+# *********************************************************************************
+# ***                                                                           ***
+# ***  Reamp exporter                                                           ***
+# ***                                                                           ***
+# ***  (c) DL2SBA Dietmar Krause 2026                                           ***
+# ***                                                                           ***
+# *********************************************************************************
+import argparse
 import struct
 import csv
 import locale
 import sys
 import os
+import logging
 from datetime import datetime
 from datetime import timezone
+from pathlib import Path
 
-print("*********************************************************************************")
-print("***                                                                           ***")
-print("*** simple REAMP logger file converter to CSV         Version 1.0             ***")
-print("***                                                                           ***")
-print("*** (c) Dietmar Krause, DL2SBA 2026                   2026-02-20              ***")
-print("***                                                                           ***")
-print("*** Usage                                                                     ***")
-print("***      python scriptName <parm1> <parm2> <parm3>                            ***")
-print("***                                                                           ***")
-print("***      parm1    locale used for number formatting                           ***")
-print("***               'German' for ',' as decimal seperator                       ***")
-print("***               'US' for '.' as decimal seperator                           ***")
-print("***      parm2    Input filename in *.reamp format                            ***")
-print("***      parm3    Output filename in CSV-format                               ***")
-print("***               Field seperator is ';'                                      ***")
-print("***               Field#                                                      ***")
-print("***               1   relative time in µs to start of recording               ***")
-print("***               2   absolute time of recording in UTC                       ***")
-print("***                   the time is fully qualifier down to µs                  ***")
-print("***               3   first channel in file                                   ***")
-print("***               4   second channel in file (optional)                       ***")
-print("***               5   third channel in file (optional)                        ***")
-print("***                                                                           ***")
-print("***               voltage data is written in mV                               ***")
-print("***               current data is written in µA                               ***")
-print("***                                                                           ***")
-print("***  Restrictions:                                                            ***")
-print("***      ChannelMap is not used                                               ***")
-print("***                                                                           ***")
-print("***  Import into Excel                                                        ***")
-print("***      The timestamp in the file is down to µs resolution.                  ***")
-print("***      Excel currently supports only ms resolution for timestamps           ***")
-print("***      To see at least ms in the column, use the custom cell format         ***")
-print("***                'TT.MM.JJJJ hh:mm:ss,000'                                  ***")
-print("***                                                                           ***")
-print("***  Send comments and bug reports to 'labview@dl2sba.de'                     ***")
-print("***                                                                           ***")
-print("*********************************************************************************")
-
-if len(sys.argv) < 4:
-    print("Incorrect usage")
-    sys.exit(-1)
-
-locale2use = sys.argv[1]
-inputFilename = sys.argv[2]
-outputFilename = sys.argv[3]
-
-numChannels = 0
-locale.setlocale(locale.LC_ALL, locale2use) 
-
-with open(inputFilename, 'rb') as f:
-    binary_content = f.read(0x200) 
-
-    fileVersion         = struct.unpack('<h', binary_content[0:2])[0]
-    fileHeaderSize      = struct.unpack('<h', binary_content[2:4])[0]
-    fileChannelCount    = struct.unpack('B', binary_content[8:9])[0]
-    fileChannelMap      = struct.unpack('B', binary_content[9:10])[0]
-    fileSampleTime      = struct.unpack('<h', binary_content[10:12])[0] / 10 # in ms ticks
-    fileType            = struct.unpack('B', binary_content[8:9])[0]
-    fileStartTS         = struct.unpack('<Q', binary_content[0x1f8:0x200])[0] / 1000
+PARM_TIME_RELATIVE = "relative"
+PARM_TIME_TIMESTAMP = "timestamp"
+PARM_TIME_MICROS = "unix"
 
 
-    print("fileVersion......", fileVersion)
-    print("fileHeaderSize...", fileHeaderSize)
-    print("fileChannelCount.",fileChannelCount)
-    print("fileChannelMap...",fileChannelMap)
-    print("fileSampleTime...",fileSampleTime,"ms")
-    print("fileType.........",fileType)
-    print("fileStartTS......",datetime.fromtimestamp(fileStartTS, tz=timezone.utc),fileStartTS, "s")
-    print("Input filename...", inputFilename)
-    print("Output filename..", outputFilename)
-    print("Output locale....", locale2use)
+# *********************************************************************************
+# ***                                                                           ***
+# ***  Exporter                                                                 ***
+# ***                                                                           ***
+# *********************************************************************************
+def process_reamp_data(
+    parm_input_path,
+    parm_output_path,
+    parm_delimiter,
+    parm_encoding,
+    parm_time,
+):
+    num_channels = 0
+    try:
+        with open(parm_input_path, "rb") as f:
+            #   process header section
+            binary_content = f.read(0x200)
 
-    with open(outputFilename, "w", newline="", encoding="utf-8") as outFile:
-        writer = csv.writer(outFile, delimiter=";")
-        writer.writerow(["time rel", "time abs", "channel 0", "channel 1", "channel 2"])
+            fileVersion = struct.unpack("<h", binary_content[0:2])[0]
+            fileHeaderSize = struct.unpack("<h", binary_content[2:4])[0]
+            fileChannelCount = struct.unpack("B", binary_content[8:9])[0]
+            fileChannelMap = struct.unpack("B", binary_content[9:10])[0]
+            fileSampleTime = (
+                struct.unpack("<h", binary_content[10:12])[0] / 10
+            )  # in ms ticks
+            fileType = struct.unpack("B", binary_content[8:9])[0]
+            fileStartTS = struct.unpack("<Q", binary_content[0x1F8:0x200])[0] / 1000
 
-        #   detector for end of file
-        #   increments are in ms
-        lastIncrement = -1
-        increment = 0
-        bytesPerChannel = (fileChannelCount + 1 ) * 8
-        timestampFromIncrement = ""
-        numSamples = 0
-        #
-        #   read the data lines
-        while True:
-            valueArray = []
-            #   read all data of a sample
-            sampleRaw = f.read(bytesPerChannel)
-            #   read 8 byte double value
-            doubleIncrement = struct.unpack('<d', sampleRaw[0:8])[0]
-            #   convert to ms
-            increment = doubleIncrement
-            #print(increment)
-            if ( increment == lastIncrement):
-                break
-            else:
-                lastIncrement = increment
-            incrementLocal = locale.format_string("%f", increment)
-            valueArray.append(incrementLocal)
+            logging.info(f"fileVersion...... {fileVersion}")
+            logging.info(f"fileHeaderSize... {fileHeaderSize}")
+            logging.info(f"fileChannelCount. {fileChannelCount}")
+            logging.info(f"fileChannelMap... {fileChannelMap}")
+            logging.info(f"fileSampleTime... {fileSampleTime}ms")
+            logging.info(f"fileType......... {fileType}")
+            logging.info(f"fileStartTS...... {fileStartTS}s")
+            logging.info(
+                f"                  {datetime.fromtimestamp(fileStartTS, tz=timezone.utc)}"
+            )
 
-            #   timestamp of current sample in ms
-            sampleTime = fileStartTS + increment
-            timestampFromIncrement = datetime.fromtimestamp(sampleTime, tz=timezone.utc)
-            #print(sampleTime, timestampFromIncrement)
-            valueArray.append(timestampFromIncrement)
+            #   process data records
+            with open(
+                parm_output_path, "w", newline="", encoding=parm_encoding
+            ) as outFile:
+                writer = csv.writer(outFile, delimiter=parm_delimiter)
+                writer.writerow(["time", "channel 0", "channel 1", "channel 2"])
 
-            for chanNo in range(fileChannelCount):
-                startIdx = chanNo * 8 + 8
-                stopIdx  = startIdx + 8
-                chanVal = struct.unpack('<d', sampleRaw[startIdx:stopIdx])[0]
-                chanValLocale = locale.format_string("%f", chanVal)
-                valueArray.append(chanValLocale)
-            writer.writerow(valueArray)
-            numSamples += 1
+                #   detector for end of file
+                #   increments are in ms
+                lastIncrement = -1
+                increment = 0
+                bytesPerChannel = (fileChannelCount + 1) * 8
+                timestampFromIncrement = ""
+                numSamples = 0
+                #
+                #   read the data lines
+                while True:
+                    valueArray = []
+                    #   read all data of a sample
+                    sampleRaw = f.read(bytesPerChannel)
+                    #   read 8 byte double value
+                    doubleIncrement = struct.unpack("<d", sampleRaw[0:8])[0]
+                    #   convert to ms
+                    increment = doubleIncrement
+                    # print(increment)
+                    if increment == lastIncrement:
+                        break
+                    else:
+                        lastIncrement = increment
 
-    print("fileLastTS.......", timestampFromIncrement)
-    print("samples written..", numSamples)
-    print("bye ...")
+                    timestampFromIncrement = fileStartTS + increment
+
+                    if parm_time == PARM_TIME_RELATIVE:
+                        valueArray.append(locale.format_string("%f", increment))
+                    elif parm_time == PARM_TIME_TIMESTAMP:
+                        #   timestamp of current sample in ms
+                        valueArray.append(
+                            datetime.fromtimestamp(
+                                timestampFromIncrement, tz=timezone.utc
+                            )
+                        )
+                    else:
+                        #   timestamp of current sample in s
+                        valueArray.append(timestampFromIncrement)
+
+                    for chanNo in range(fileChannelCount):
+                        startIdx = chanNo * 8 + 8
+                        stopIdx = startIdx + 8
+                        chanVal = struct.unpack("<d", sampleRaw[startIdx:stopIdx])[0]
+                        chanValLocale = locale.format_string("%f", chanVal)
+                        valueArray.append(chanValLocale)
+                    writer.writerow(valueArray)
+                    numSamples += 1
+
+            logging.info(f"fileLastTS....... {timestampFromIncrement}")
+            logging.info(f"                  {datetime.fromtimestamp(
+                            timestampFromIncrement, tz=timezone.utc
+                        )}")
+            logging.info(f"samples written.. {numSamples}")
+            logging.info("... bye ...")
+
+    except FileNotFoundError as e:
+        logging.error(f"Error: File '{e.filename}' not found", file=sys.stderr)
+
+    except Exception as e:
+        logging.error(f"Ein unerwarteter Fehler ist aufgetreten: {e}", file=sys.stderr)
+
+
+# *********************************************************************************
+# ***                                                                           ***
+# ***  Main                                                                     ***
+# ***                                                                           ***
+# *********************************************************************************
+def main():
+    logging.basicConfig(
+        level=logging.WARNING, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+    # init Parser
+    parser = argparse.ArgumentParser(
+        description="Converts Reamp-Datafile into CSV-Format."
+    )
+
+    # mandatory parameter input filename
+    parser.add_argument("input_file", help="filename of *.reamp datafile")
+
+    # optional output file name
+    parser.add_argument(
+        "-o", "--output", help="Name of the output file (default: <inputfile>.csv)"
+    )
+
+    # optional locale
+    parser.add_argument(
+        "-l",
+        "--locale",
+        default="German",
+        help="Locale used for number formatting (default: 'German')",
+    )
+
+    # optional output encoding
+    parser.add_argument(
+        "-e",
+        "--encoding",
+        default="utf-8",
+        help="Encoding of output file (default: 'utf-8')",
+    )
+
+    # optional verbose
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Verbose log output"
+    )
+
+    # optional delimeter
+    parser.add_argument(
+        "-d",
+        "--delimiter",
+        choices=[",", ";"],
+        default=";",
+        help="Value delimiter in CSV-file (default: ';')",
+    )
+
+    # optional mode of time column
+    parser.add_argument(
+        "-t",
+        "--time",
+        choices=[PARM_TIME_MICROS, PARM_TIME_RELATIVE, PARM_TIME_TIMESTAMP],
+        default=PARM_TIME_RELATIVE,
+        help=f"Time column in CSV (default: {PARM_TIME_RELATIVE})",
+    )
+
+    # parse args
+    args = parser.parse_args()
+
+    input_file_name = Path(args.input_file)
+    output_file_name = (
+        args.output if args.output else input_file_name.with_suffix(".csv")
+    )
+    locale_used = args.locale
+    delimiter_used = args.delimiter
+    encoding_used = args.encoding
+    time_used = args.time
+
+    if args.verbose:
+        # CRITICAL	50
+        # ERROR	40
+        # WARNING	30
+        # INFO	20
+        # DEBUG	10
+        logging.getLogger().setLevel(20)
+
+    logging.info(f"*********************************************************************************")
+    logging.info(f"***                                                                           ***")
+    logging.info(f"***  Reamp exporter                                                           ***")
+    logging.info(f"***                                                                           ***")
+    logging.info(f"***    Repository  https://github.com/dl2sba/Reamp2CSV                        ***")
+    logging.info(f"***    Homepage    https://dl2sba.com                                         ***")
+    logging.info(f"***                                                                           ***")
+    logging.info(f"***  (c) DL2SBA Dietmar Krause 2026                                           ***")
+    logging.info(f"***                                                                           ***")
+    logging.info(f"*********************************************************************************")
+    logging.info(f"input filename... {input_file_name}")
+    logging.info(f"output filename.. {output_file_name}")
+    logging.info(f"locale used...... {locale_used}")
+    logging.info(f"data delimiter... {delimiter_used}")
+    logging.info(f"output encoding.. {encoding_used}")
+    logging.info(f"time columns .... {time_used}")
+
+    locale.setlocale(locale.LC_ALL, locale_used)
+
+    process_reamp_data(
+        input_file_name, output_file_name, delimiter_used, encoding_used, time_used
+    )
+
+
+# *********************************************************************************
+# ***                                                                           ***
+# ***  Launcher                                                                 ***
+# ***                                                                           ***
+# *********************************************************************************
+if __name__ == "__main__":
+    main()
